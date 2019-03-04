2c8858e6b7ae171c59e17d7bf9abbc50c5e1ef2b
nanp <nanp@ethz.ch>
2019-03-04 13:03:01 +0100

    Delete readinteger.R

--- CALISTA-R/R/readinteger.R
+++
@@ -1,8 +0,0 @@
-readinteger<-function(){
-  #print(paste('Optimal number of cluster according to max. eigenvalue',
-  #            toString(x) ))
-  #print("If you want to use this value enter 'y' " )
-  n=readLines(n=1)   
-  #return(as.integer(n))
-  return(n)
-}
